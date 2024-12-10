18c6e22bb255c6f12a81d95bba335d12c35baa3a
Harwinder Singh <harwinder61@gmail.com>
2024-12-10 10:35:04 +0530

    chache clear

--- Modules/Escort/Http/Controllers/EscortController.php
+++ Modules/Escort/Http/Controllers/EscortController.php
@@ -135,27 +135,6 @@ public function profileViews($id, Request $request)
 }
 
 
-// public function profileViews($id, Request $request)
-// {
-//     $user = auth()->user();
-    
-//     if ($user->user_type != 1) {    
-//         return Resp::error(['message' => 'Unauthorized user not a fan']);
-//     }
-
-//     $profile = AuthUser::where('id', $id)
-//                        ->where('user_type', 2)
-//                        ->with('profile')
-//                        ->first();
-
-//     if (!$profile || !$profile->profile) {
-//         return Resp::error(['message' => 'User is not an escort or profile not found']);
-//     }
-
-//     $profile->profile->increment('profile_views');
-
-//     return Resp::success(['message' => 'Profile views updated successfully']);
-// }
     public function hideProfile(Request $request)
     {
         $validator = Validator::make($request->all(), [
