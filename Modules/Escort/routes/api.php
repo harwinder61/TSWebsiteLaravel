bd7542c410206f3ccbff331c45d3c30adecc0af0
Harwinder Singh <harwinder61@gmail.com>
2024-12-10 18:40:59 +0530

    create new api

--- Modules/Escort/routes/api.php
+++ Modules/Escort/routes/api.php
@@ -32,6 +32,7 @@ Route::group(['prefix' => 'escort'],function(){
     Route::put('/orders',[OrderController::class,'updateOrder']);
     Route::get('/active-subscription',[EscortController::class,'getActiveSubscription']);
     Route::post('/verify',[EscortController::class,'verify']);
+    Route::get('/get-verify',[EscortController::class,'getVerify']);
     Route::get('/featured-ts-girl',[EscortController::class,'featuredTsGirl']);
     Route::get('/previous-subscriptions',[OrderController::class,'getEscortPreviousSubscriptions']);
     Route::get('/latest-subscription',[OrderController::class,'getLatestEscortSubscription']);
