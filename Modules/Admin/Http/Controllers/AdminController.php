53d91e74be058058a1eb36468730564fdbb223da
Harwinder Singh <harwinder61@gmail.com>
2025-01-02 12:29:56 +0530

    Minor fixes

--- Modules/Admin/Http/Controllers/AdminController.php
+++ Modules/Admin/Http/Controllers/AdminController.php
@@ -112,7 +112,9 @@ class AdminController extends Controller
 
     public function profileUpdateMedia($id, Request $request)
     {
-        // Validate the input
+        try{
+
+            // Validate the input
         $validator = Validator::make($request->all(), [
             'gallery' => 'array',
             'gallery.*' => 'exists:media,id',
@@ -127,7 +129,7 @@ class AdminController extends Controller
         }
     
         // Find the profile
-        $user = Profile::find($id);
+        $user = Profile::where('escort_id', $id)->first();
         if (!$user) {
             return Resp::error(['message' => 'User not found']);
         }
@@ -187,8 +189,19 @@ class AdminController extends Controller
             $user->is_media = 1;
             $user->save();
         }
+
+        $media=Media::where('escort_id', $id)->first();
+        if(!$media){
+            return Resp::error(['message' => 'Media not found']);
+        }
+        $user->load('media');
+
     
-        return Resp::success(['message' => 'Media updated successfully']);
+        return Resp::success(['message' => 'Media updated successfully','media'=>$media,'profile'=>$user]);
+
+        }catch(\Exception $e){
+            return Resp::error(['message' => $e->getMessage()]);
+        }
     }
 
 
@@ -1215,7 +1228,8 @@ public function getVarifiacationList(Request $request)
 
         // Filter by verified status if provided
         if ($request->has('verified_status')) {
-            $verifiedStatus = explode(',', $request->query('verified_status'));
+            //$verifiedStatus = explode(',', $request->query('verified_status'));
+            $verifiedStatus = $request->query('verified_status');
             $query->whereIn('verified_status', $verifiedStatus);
         } else {
             // Default to verified statuses 1 and 4 if not provided
