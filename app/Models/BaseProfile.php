bf6b088a7bea0894180debf0161c07f7872c5ae6
Harwinder Singh <me@harwindersingh.com>
2025-03-17 19:22:23 +0530

    remove validation

--- app/Models/BaseProfile.php
+++ app/Models/BaseProfile.php
@@ -92,7 +92,7 @@ class BaseProfile extends Model
         'phone_number' => 'required|string',
         'allow_whatsapp' => 'boolean',
         //'gender' => 'required|in:'.implode(',',Gender::getValues()),
-        'date_of_birth' => 'string',
+        'date_of_birth' => 'nullable',
         //'orientation'=>'required|in:'.implode(',',Orientation::getValues()),
         //'ethnicity'=>'required|in:'.implode(',',Ethnicity::getValues()),
         'ethnicity' => 'required',
